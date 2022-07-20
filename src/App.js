f8d5e6f650f9574f97e5d6f273b6bc783237de4c
pedrorocha-loggi <pedro.rocha@loggi.com>
2022-07-20 11:34:53 -0300

    implement step 1 branch

--- src/App.js
+++ src/App.js
@@ -1,92 +1,38 @@
-import { AppBar as MuiAppBar, Box, Button, Card, CardActions, CardContent, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, TextField, Toolbar, Typography } from "@material-ui/core";
-import { Add, Delete } from "@material-ui/icons";
-import { useState } from "react";
-
-function NewNoteDialog({ showAddDialog, setShowAddDialog, setNotes }) {
-  let newNote = '';
-  return (
-    <Dialog
-      open={showAddDialog}
-      onClose={() => setShowAddDialog(false)}
-      aria-labelledby="add-dialog-title"
-      aria-describedby="add-dialog-description"
-    >
-      <DialogTitle id="add-dialog-title">Nova anotação</DialogTitle>
-      <DialogContent>
-        <TextField onChange={event => newNote = event.target.value} />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={() => setShowAddDialog(false)}>
-          Cancelar
-        </Button>
-        <Button onClick={() => {
-          setNotes(prevNotes => prevNotes.concat(newNote));
-          setShowAddDialog(false);
-        }}>
-          Adicionar
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-}
-
-function AppBar({ setShowAddDialog }) {
-  return (
-    <MuiAppBar position="sticky">
-      <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          Anotações
-        </Typography>
-        <IconButton style={{ color: 'white' }} onClick={() => setShowAddDialog(true)}>
-          <Add />
-        </IconButton>
-      </Toolbar>
-    </MuiAppBar>
-  );
-}
-
-function Note({ index, content, setNotes }) {
-  return (
-    <Card style={{ marginTop: 10, marginRight: 10, marginLeft: 10 }}>
-      <CardContent>
-        <Typography>{content}</Typography>
-      </CardContent>
-      <CardActions style={{ flexDirection: 'row-reverse' }}>
-        <Button
-          size="small"
-          color="primary"
-          onClick={() => setNotes(prevNotes => {
-            const newNotes = prevNotes.slice();
-            newNotes.splice(index, 1);
-            return newNotes;
-          })}
-          startIcon={<Delete />}
-        >
-          Apagar
-        </Button>
-      </CardActions>
-    </Card>
-  );
-}
+import './App.css';
+import { AppBar, Box, Button, Card, CardActions, CardContent, IconButton, Toolbar, Typography } from '@mui/material';
+import { Add, Delete } from '@mui/icons-material';
 
 function App() {
-  const [notes, setNotes] = useState(
-    Array(10).fill('Conteúdo').map((content, index) => `${content} ${index}`));
-  const [showAddDialog, setShowAddDialog] = useState(false);
-
+  const notes = Array(10).fill('Conteúdo').map(
+    (content, index) => `${content} ${index}`);
   return (
     <Box>
-      <NewNoteDialog
-        showAddDialog={showAddDialog}
-        setShowAddDialog={setShowAddDialog}
-        setNotes={setNotes}
-      />
-      <AppBar setShowAddDialog={setShowAddDialog} />
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography flexGrow={1}>Minhas anotações</Typography>
+          <IconButton color='inherit' >
+            <Add />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      {notes.map((note, index) => (
+        <Card key={index} sx={{ margin: 1 }}>
+          <CardContent>
+            <Typography>{note}</Typography>
+          </CardContent>
+          <CardActions sx={{ flexDirection: 'row-reverse' }}>
+            <Button
+              size="small"
+              color="primary"
+              startIcon={<Delete />}
+            >
+              Apagar
+            </Button>
+          </CardActions>
+        </Card>
 
-      {notes.map((content, index) => (
-        <Note content={content} index={index} setNotes={setNotes} key={index} />
       ))}
-    </Box >
+    </Box>
   );
 }
 
